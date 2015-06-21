c333e6a799e6a57c943b31213dd377767af3645a
Jeffrey Grinberg <grinbergjeff@gmail.com>
2015-06-21 17:11:57 -0400

    Checking if commit works

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function(){
-	
+/* Template Code Below */	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -10,8 +10,16 @@ $(document).ready(function(){
   	/*--- Hide information modal box ---*/
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
+
   	});
-
+/*Template Code Above*/
+	
+/*Code by Jeffrey Grinberg below: */
+	newGame();
 });
-
-
+//Allows users to start a new game
+function newGame() {
+	
+	
+	
+}
